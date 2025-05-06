bf6061221dd616db087b55a91bcc4624130d95ee
Андрей Максимович <gorshkov5d4535@gmail.com>
2025-05-06 23:39:34 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Дмитрий Мордас',
+        name: 'Зайчик',
+        publicationYear: 2025,
+        price: 450,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Сайконо Джокер',
+        name: 'Зайчик: Любовь, смерть и клубника',
+        publicationYear: 2023,
+        price: 680,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Грег Кокс',
+        name: 'Бэтмен. Суд сов',
+        publicationYear: 2020,
+        price: 890,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Криста Фауст',
+        name: 'Бэтмен. Убийственная шутка',
+        publicationYear: 2019,
+        price: 920, 
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Влад Райбер',
+        name: 'Летний лагерь',
+        publicationYear: 2025,
+        price: 399,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Собачье сердце',
+        publicationYear: 1925,
+        price: 350,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Александр Пушкин',
+        name: 'Капитанская дочка',
+        publicationYear: 1836,
+        price: 290,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Михаил Шолохов',
+        name: 'Судьба человека',
+        publicationYear: 1956,
+        price: 420,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Михаил Лермонтов',
+        name: 'Герой нашего времени',
+        publicationYear: 1840,
+        price: 380,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Николай Гоголь',
+        name: 'Шинель',
+        publicationYear: 1842,
+        price: 310,
+        bestseller: false,
+        available: true
+    },
+];
